b4184c6095c2c043bf8d5094eff4c22a4898c9d0
Abhishek Pratap Singh <navodayanabhishek@gmail.com>
2021-09-18 09:33:11 +0530

    first commit

---
+++ feed/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from accounts import views as accounts_views
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='feed-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='feed-about'),
+    path('profile/<str:username>', accounts_views.profile, name='profile'),
+    path('post-upvote/<int:pk>', views.PostUpvote, name='post-upvote'),
+    path('post-downvote/<int:pk>', views.PostDownvote, name='post-downvote'),
+]
